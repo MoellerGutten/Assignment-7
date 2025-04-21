92cc70da84d35a09cdf4140755553e1414945b50
MoellerGutten <93318104+MoellerGutten@users.noreply.github.com>
2025-04-21 20:38:06 +0200

    Clean up

--- game-sign-up-client/src/main/java/dk/dtu/compute/course02324/part4/consuming_rest/GameSignUpApp.java
+++ game-sign-up-client/src/main/java/dk/dtu/compute/course02324/part4/consuming_rest/GameSignUpApp.java
@@ -5,7 +5,6 @@ import dk.dtu.compute.course02324.part4.consuming_rest.model.Player;
 import dk.dtu.compute.course02324.part4.consuming_rest.wrappers.HALWrapperGames;
 import dk.dtu.compute.course02324.part4.consuming_rest.wrappers.HALWrapperPlayers;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -14,20 +13,15 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
 import javafx.util.converter.IntegerStringConverter;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.SerializationUtils;
 import org.springframework.web.client.RestClient;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -61,15 +55,8 @@ public class GameSignUpApp extends Application {
                     baseUrl("http://localhost:8080").
             build();
 
-    /**
-     * The method starting the application, which sets up the GUI
-     * elements of this application.
-     *
-     * @param stage the stage for this application (provided by JavaFX)
-     * @throws Exception if something should go wrong (required by super class)
-     */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         this.stage = stage;
 
         root = new Pane();
@@ -77,49 +64,7 @@ public class GameSignUpApp extends Application {
         Button addNewGameButton = new Button("Add Game");
 
         addNewGameButton.setOnAction(eee -> {
-            Label gameNameLabel = new Label("Enter a name for the game: ");
-            Label minimumLabel = new Label("Minimum amount of players: ");
-            Label maximumLabel = new Label("Maximum amount of players: ");
-
-            TextField gameNameField = new TextField("Default");
-            TextField maximumPlayersField = new TextField("2");
-            TextField minimumPlayersField = new TextField("2");
-
-            maximumPlayersField.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
-            minimumPlayersField.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
-
-            Button createGameButton = new Button("Create game");
-            VBox box = new VBox(root, gameNameLabel, gameNameField, minimumLabel, minimumPlayersField, maximumLabel, maximumPlayersField, createGameButton);
-            Scene scene = new Scene(box, 200, 250);
-            Stage createStage = new Stage();
-            createStage.setScene(scene);
-            createStage.setTitle("Creating game");
-            createStage.setResizable(false);
-            createStage.sizeToScene();
-            createStage.show();
-
-            createGameButton.setOnAction(eeee -> {
-                try {
-                    String name = gameNameField.getText();
-                    int minPlayers = Integer.parseInt(minimumPlayersField.getText());
-                    int maxPlayers = Integer.parseInt(maximumPlayersField.getText());
-                    Game game = new Game();
-                    game.setName(name);
-                    game.setMinPlayers(minPlayers);
-                    game.setMaxPlayers(maxPlayers);
-                    ResponseEntity<Game> gameResponse = customClient.post()
-                            .uri("/game").accept(MediaType.APPLICATION_JSON)
-                            .body(game).retrieve().toEntity(new ParameterizedTypeReference<Game>() {
-                            });
-
-                    createStage.close();
-                    update();
-                } catch (Exception exception) {
-
-                }
-
-            });
-
+            openAddGameMenu();
         });
 
         gamesPane = new GridPane();
@@ -132,11 +77,8 @@ public class GameSignUpApp extends Application {
         ScrollPane scrollPane = new ScrollPane(gamesPane);
         scrollPane.setMinWidth(300);
         scrollPane.setMaxWidth(300);
-        scrollPane.setMinHeight(300);
-        scrollPane.setMaxHeight(300);
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-
 
         VBox box = new VBox(root, addNewGameButton, scrollPane);
         Scene scene = new Scene(box);
@@ -147,7 +89,6 @@ public class GameSignUpApp extends Application {
         this.stage.setResizable(false);
         this.stage.sizeToScene();
         this.stage.show();
-
     }
 
     void update() {
@@ -160,67 +101,47 @@ public class GameSignUpApp extends Application {
             String gameName = game.getName();
             int minPlayers = game.getMinPlayers();
             int maxPlayers = game.getMaxPlayers();
+            // Long request for getting list of players for each game. This is converted to a list of names to display.
+            List<String> playersPerGame = customClient.get().uri("/game/"+ game.getUid() +"/players")
+                    .retrieve().body(HALWrapperPlayers.class).getPlayers()
+                    .stream().map(Player::getName).toList();
 
-            List<String> playersPerGame = customClient.get().uri("/game/"+ game.getUid() +"/players").retrieve().body(HALWrapperPlayers.class).getPlayers().stream().map(Player::getName).toList();
-            System.out.println(playersPerGame);
-
-            List<Player> players = customClient.get().uri("/player").retrieve().body(HALWrapperPlayers.class).getPlayers();
-
-            Label playerListLabel = new Label(playersPerGame == null ? "[]" : playersPerGame.toString());
-            Label playerListLabel2 = new Label(playersPerGame == null ? "[]" : playersPerGame.toString());
-
-            gameInfoBox = new VBox(root, new Label("game: " + gameName), new Label("Minimum players: " + minPlayers), new Label("Maximum players: " + maxPlayers), playerListLabel);
+            gameInfoBox = new VBox(root,
+                    new Label("game: " + gameName),
+                    new Label("Minimum players: " + minPlayers),
+                    new Label("Maximum players: " + maxPlayers),
+                    new Label(playersPerGame.toString()));
             gameInfoBox.setSpacing(3);
             gameInfoBox.setPadding(new Insets(4, 8, 4, 8));
-            gameInfoBox.setStyle("-fx-border-color: black;\n" + "-fx-border-width: 2;\n" + "-fx-background-color: lightgrey;\n" + "-fx-border-radius: 4;");
+            gameInfoBox.setStyle("""
+                    -fx-border-color: black;
+                    -fx-border-width: 2;
+                    -fx-background-color: lightgrey;
+                    -fx-border-radius: 4;""");
 
-            gameInfoBox2 = new VBox(root, new Label("game: " + gameName), new Label("Minimum players: " + minPlayers), new Label("Maximum players: " + maxPlayers), playerListLabel2);
+            gameInfoBox2 = new VBox(root,
+                    new Label("game: " + gameName),
+                    new Label("Minimum players: " + minPlayers),
+                    new Label("Maximum players: " + maxPlayers),
+                    new Label(playersPerGame.toString()));
             gameInfoBox2.setSpacing(3);
             gameInfoBox2.setPadding(new Insets(4, 8, 4, 8));
-            gameInfoBox2.setStyle("-fx-border-color: black;\n" + "-fx-border-width: 2;\n" + "-fx-background-color: lightgrey;\n" + "-fx-border-radius: 4;");
+            gameInfoBox2.setStyle("""
+                    -fx-border-color: black;
+                    -fx-border-width: 2;
+                    -fx-background-color: lightgrey;
+                    -fx-border-radius: 4;""");
 
             infoBoxes.add(gameInfoBox);
             infoBoxes2.add(gameInfoBox2);
 
             Button signUpButton = new Button("Sign Up!");
+            // Copy for lambda for some reason
+            int finalI = i;
             signUpButton.setOnAction(e -> {
-                Label userLabel = new Label("User: ");
-                Label playerLabel = new Label("Player: ");
-
-                TextField userField = new TextField("");
-                TextField playerField = new TextField("");
-
-                HBox userHbox = new HBox(userLabel, userField);
-                HBox playerHbox = new HBox(playerLabel, playerField);
-
-                Button signUpInsideButton = new Button("Sign Up");
-
-                VBox box = new VBox(root, infoBoxes2.get((int) game.getUid() - 1), userHbox, playerHbox, signUpInsideButton);
-                Scene signUpScene = new Scene(box, 200, 250);
-                Stage signUpStage = new Stage();
-                signUpStage.setScene(signUpScene);
-                signUpStage.setTitle("Signing up for game");
-                signUpStage.setResizable(false);
-                signUpStage.sizeToScene();
-                signUpStage.show();
-                signUpInsideButton.setOnAction(ee -> {
-                    String body = "http://localhost:8080/game/" + game.getUid();
-                    int playerUID = -1;
-                    for (Player player : players) {
-                        if (Objects.equals(player.getName(), playerField.getText())) {
-                            playerUID = (int) player.getUid();
-                        }
-                    }
-                    if (playerUID != -1) {
-                        ResponseEntity<Player> playerResponseEntity = customClient.put().uri("/player/"+ playerUID +"/game").
-                                header("Content-Type", "text/uri-list").
-                                body(body).retrieve().toEntity(Player.class);
-                        System.out.println("Trying to do patch");
-                    }
-                    update();
-                    signUpStage.close();
-                });
+                openSignUpMenu(game, finalI);
             });
+
             HBox entry = new HBox(root, infoBoxes.get(i), signUpButton);
             entry.setSpacing(5.0);
             entry.setAlignment(Pos.CENTER);
@@ -229,6 +150,93 @@ public class GameSignUpApp extends Application {
         }
     }
 
+    void openAddGameMenu() {
+        Label gameNameLabel = new Label("Enter a name for the game: ");
+        Label minimumLabel = new Label("Minimum amount of players: ");
+        Label maximumLabel = new Label("Maximum amount of players: ");
+
+        TextField gameNameField = new TextField("Default");
+        TextField maximumPlayersField = new TextField("2");
+        TextField minimumPlayersField = new TextField("2");
+
+        maximumPlayersField.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
+        minimumPlayersField.setTextFormatter(new TextFormatter<>(new IntegerStringConverter()));
+
+        Button createGameButton = new Button("Create game");
+        VBox box = new VBox(root, gameNameLabel, gameNameField, minimumLabel, minimumPlayersField, maximumLabel, maximumPlayersField, createGameButton);
+        Scene scene = new Scene(box, 200, 250);
+        Stage createStage = new Stage();
+        createStage.setScene(scene);
+        createStage.setTitle("Creating game");
+        createStage.setResizable(false);
+        createStage.sizeToScene();
+        createStage.show();
+
+        createGameButton.setOnAction(eeee -> {
+            try {
+                String name = gameNameField.getText();
+                int minPlayers = Integer.parseInt(minimumPlayersField.getText());
+                int maxPlayers = Integer.parseInt(maximumPlayersField.getText());
+                Game game = new Game();
+                game.setName(name);
+                game.setMinPlayers(minPlayers);
+                game.setMaxPlayers(maxPlayers);
+                customClient.post()
+                        .uri("/game").accept(MediaType.APPLICATION_JSON)
+                        .body(game).retrieve().toEntity(new ParameterizedTypeReference<Game>() {
+                        });
+
+                createStage.close();
+                update();
+            } catch (Exception exception) {
+                // should be handled better
+                System.out.println("Invalid input");
+            }
+        });
+    }
+
+    void openSignUpMenu(Game game, int i) {
+        List<Player> players = customClient.get().uri("/player").retrieve().body(HALWrapperPlayers.class).getPlayers();
+
+        Label userLabel = new Label("User: ");
+        Label playerLabel = new Label("Player: ");
+
+        TextField userField = new TextField("");
+        TextField playerField = new TextField("");
+
+        HBox userHbox = new HBox(userLabel, userField);
+        HBox playerHbox = new HBox(playerLabel, playerField);
+
+        Button signUpInsideButton = new Button("Sign Up");
+
+        VBox box = new VBox(root, infoBoxes2.get(i), userHbox, playerHbox, signUpInsideButton);
+
+        Scene signUpScene = new Scene(box, 200, 250);
+        Stage signUpStage = new Stage();
+        signUpStage.setScene(signUpScene);
+        signUpStage.setTitle("Signing up for game");
+        signUpStage.setResizable(false);
+        signUpStage.sizeToScene();
+        signUpStage.show();
+
+        signUpInsideButton.setOnAction(ee -> {
+            String body = "http://localhost:8080/game/" + game.getUid();
+            int playerUID = -1;
+            for (Player player : players) {
+                if (Objects.equals(player.getName(), playerField.getText())) {
+                    playerUID = (int) player.getUid();
+                }
+            }
+            if (playerUID != -1) {
+                customClient.put().uri("/player/"+ playerUID +"/game").
+                        header("Content-Type", "text/uri-list").
+                        body(body).retrieve().toEntity(Player.class);
+                System.out.println("Trying to do patch");
+            }
+            update();
+            signUpStage.close();
+        });
+    }
     /**
      * The main method used to start the JavaFX application.
      * @param args the command line arguments
